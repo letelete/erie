15a0512b234ee639b2eb921a771cd77c57d59905
letelete <brunokawka@gmail.com>
2019-03-30 17:49:13 +0100

    Feat: Connect HumanDate with adapter to show article publishedAt attr

--- app/src/main/java/io/erie/ui/global/article/ArticleAdapter.kt
+++ app/src/main/java/io/erie/ui/global/article/ArticleAdapter.kt
@@ -5,16 +5,17 @@ import androidx.paging.PagedListAdapter
 import io.erie.R
 import io.erie.commons.extensions.inflate
 import io.erie.commons.utils.ArticleDiffUtilCallback
+import io.erie.commons.utils.HumanDate
 import io.erie.model.entities.ArticleEntity
-import io.erie.model.responses.Article
+import javax.inject.Inject
 
-class ArticleAdapter
+class ArticleAdapter @Inject constructor(private val humanDate: HumanDate)
     : PagedListAdapter<ArticleEntity,
         ArticleViewHolder>(ArticleDiffUtilCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
         val rootView = parent.inflate(R.layout.item_article)
-        return ArticleViewHolder(rootView)
+        return ArticleViewHolder(rootView, humanDate)
     }
 
     override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
